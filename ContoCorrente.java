96af476872eae866efa5f7b93943a4a2add9856b
Giordano Orlacchio <112937080+Giorsano@users.noreply.github.com>
2022-11-23 16:34:43 +0100

    Add files via upload

---
+++ ContoCorrente.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+
+public class ContoCorrente {
+    private String nomeIntestario;
+    private String cognomeIntestatario;
+    private int età;
+    private double saldo = 0;
+
+    private ArrayList<Double> listaMovimenti;
+
+    public ContoCorrente(String nomeIntestario, String cognomeIntestatario, int età,  ArrayList<Double> listaMovimenti){
+        this.nomeIntestario = nomeIntestario;
+        this.cognomeIntestatario = cognomeIntestatario;
+        this.età = età;
+        this.listaMovimenti = listaMovimenti;
+    }
+
+    public String getNomeIntestario() {
+        return nomeIntestario;
+    }
+
+    public String getCognomeIntestatario() {
+        return cognomeIntestatario;
+    }
+
+    public int getEtà() {
+        return età;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public ArrayList<Double> getListaMovimenti() {
+        return listaMovimenti;
+    }
+
+    public void setNomeIntestario(String nomeIntestario) {
+        this.nomeIntestario = nomeIntestario;
+    }
+
+    public void setCognomeIntestatario(String cognomeIntestatario) {
+        this.cognomeIntestatario = cognomeIntestatario;
+    }
+
+    public void setEtà(int età) {
+        this.età = età;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setListaMovimenti(ArrayList<Double> listaMovimenti) {
+        this.listaMovimenti = listaMovimenti;
+    }
+
+
+    public void versa(int a){
+        saldo = saldo + a;
+        listaMovimenti.add((double) a);
+    }
+
+    public void preleva(int a){
+        if(saldo < a){
+            System.out.println("Non puoi prelevare un importo maggiore del tuo saldo");
+        } else {
+            saldo = saldo - a;
+        }
+
+        listaMovimenti.add((double) a);
+
+    }
+
+    public String toString(String a){
+        a = "Il conto corrente è intestato a: " + nomeIntestario + " " + cognomeIntestatario + "\n" +
+                "Età intestatario: " + età
+                + "la lista dei movimenti è: " + listaMovimenti;
+        return a;
+    }
+
+
+}
